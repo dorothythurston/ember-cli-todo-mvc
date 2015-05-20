1d91089fc2069abe2c8eb2d63b3fdf91d7cf2c6b
dorothy <dorothy.thur@gmail.com>
2015-05-19 22:50:52 -0400

    Add ability to edit todos

--- app/components/list-item.js
+++ app/components/list-item.js
@@ -2,6 +2,21 @@ export default Ember.Component.extend({
   actions: {
     editTodo: function() {
       this.set('isEditing', true);
+    },
+    acceptChanges: function() {
+      this.set('isEditing', false);
+
+      if (Ember.isEmpty(this.get('todo.title'))) {
+        this.send('removeTodo');
+      } else {
+        this.get('todo').save();
+      }
+    },
+    removeTodo: function() {
+      console.log("remove activated");
+      var todo = this.get('todo');
+      todo.deleteRecord();
+      todo.save();
     }
   }
 });
